63f833d84b0db3d3e6dc2993192e16d28599e228
0xemagic <skrullweb.dev@gmail.com>
2025-03-10 12:48:45 -0400

    add remappings

--- src/PendleSwap.sol
+++ src/PendleSwap.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PendleSwap {
+contract PendleSwap is Ownable {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
+    address constant pendleRouter = 0x888888888889758F76e7103c6CbF23ABbF58F946;
+
+    constructor() Ownable(msg.sender) {}
+
+    function setNumber(uint256 newNumber) public onlyOwner {
         number = newNumber;
     }
 
