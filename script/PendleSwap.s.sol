f1d015dba381f909af491abc5553d97611aa91d1
0xemagic <skrullweb.dev@gmail.com>
2025-03-10 11:47:42 -0400

    create PendleSwap contract

--- script/PendleSwap.s.sol
+++ script/PendleSwap.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {PendleSwap} from "../src/PendleSwap.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract PendleSwapScript is Script {
+    PendleSwap public pendleSwap;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        pendleSwap = new PendleSwap();
 
         vm.stopBroadcast();
     }
---
+++ test/PendleSwap.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {PendleSwap} from "../src/PendleSwap.sol";
+
+contract PendleSwapTest is Test {
+    PendleSwap public pendleSwap;
+
+    function setUp() public {
+        pendleSwap = new PendleSwap();
+    }
+
+    function test_PendleSwap() public view {
+        assertEq(pendleSwap.number(), 0);
+    }
+}
