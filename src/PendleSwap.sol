5dd1bf617bb738ec27ca84fbc6d041f977a4fa97
0xemagic <skrullweb.dev@gmail.com>
2025-03-13 20:49:27 -0400

    complete swap unit test script

--- src/PendleSwap.sol
+++ src/PendleSwap.sol
@@ -3,16 +3,25 @@ pragma solidity ^0.8.28;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {console2} from "forge-std/Test.sol";
+
 import {IPAllActionV3} from "pendle-core-v2-public/contracts/interfaces/IPAllActionV3.sol";
-import "pendle-core-v2-public/contracts/interfaces/IPAllActionTypeV3.sol";
 import {IPMarket} from "pendle-core-v2-public/contracts/interfaces/IPMarket.sol";
 import {IStandardizedYield} from "pendle-core-v2-public/contracts/interfaces/IStandardizedYield.sol";
 import {IPPrincipalToken} from "pendle-core-v2-public/contracts/interfaces/IPPrincipalToken.sol";
 import {IPYieldToken} from "pendle-core-v2-public/contracts/interfaces/IPYieldToken.sol";
+import {IPMarketFactoryV3} from "pendle-core-v2-public/contracts/interfaces/IPMarketFactoryV3.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "pendle-core-v2-public/contracts/interfaces/IPAllActionTypeV3.sol";
+
 
 contract PendleSwap is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
     IPAllActionV3 pendleRouter =
         IPAllActionV3(0x888888888889758F76e7103c6CbF23ABbF58F946);
+    IPMarketFactoryV3 pendleMarketFactoryV3 =
+        IPMarketFactoryV3(0x1A6fCc85557BC4fB7B534ed835a03EF056552D52);
 
     /// @notice Supported assets in
     enum TokenType {
@@ -22,6 +31,11 @@ contract PendleSwap is Ownable, ReentrancyGuard {
         YT,
         LP
     }
+    error UnsupportedMarket();
+    error UnsupportedAsset();
+    error InvalidDepositAmount();
+    error InsufficientBalance();
+    error InvalidSwap();
 
     constructor() Ownable(msg.sender) {}
 
@@ -29,124 +43,143 @@ contract PendleSwap is Ownable, ReentrancyGuard {
     mapping(address => mapping(address => uint256)) public userBalances;
     mapping(address => TokenType) public supportedAssetTypes;
     mapping(address => address) public supportedMarkets;
+    mapping(address => mapping(TokenType => address)) public marketToken;
 
     event Deposited(address indexed user, address assetType, uint256 amount);
 
-    function setSupportedAsset(
+    function setSupportedMarket(
         address asset,
-        address market,
-        TokenType assetType
+        address market
     ) external onlyOwner {
+        require(asset != address(0), UnsupportedAsset());
         (
             IStandardizedYield _SY,
             IPPrincipalToken _PT,
             IPYieldToken _YT
-        ) = IPMarket(supportedMarkets[market]).readTokens();
+        ) = IPMarket(market).readTokens();
         supportedMarkets[address(_SY)] = market;
         supportedAssetTypes[address(_SY)] = TokenType.SY;
         supportedMarkets[address(_PT)] = market;
         supportedAssetTypes[address(_PT)] = TokenType.PT;
         supportedMarkets[address(_YT)] = market;
         supportedAssetTypes[address(_YT)] = TokenType.YT;
-        if (assetType == TokenType.UNDERLYING) {
-            supportedMarkets[asset] = market;
-            supportedAssetTypes[asset] = TokenType.UNDERLYING;
-        }
+        supportedMarkets[market] = market;
+        supportedAssetTypes[market] = TokenType.LP;
+        supportedMarkets[asset] = market;
+        supportedAssetTypes[asset] = TokenType.UNDERLYING;
+        marketToken[market][TokenType.UNDERLYING] = asset;
+        marketToken[market][TokenType.SY] = address(_SY);
+        marketToken[market][TokenType.PT] = address(_PT);
+        marketToken[market][TokenType.YT] = address(_YT);
+        marketToken[market][TokenType.LP] = market;
     }
 
     /// @notice Deposits a supported asset
     /// @param inputToken The asset to deposit
-    /// @param inputTokenType The type of asset to deposit
     /// @param amount The amount to deposit
-    function deposit(
-        address inputToken,
-        TokenType inputTokenType,
-        uint256 amount
-    ) external nonReentrant {
-        require(amount > 0, "Invalid deposit amount");
+    function deposit(address inputToken, uint256 amount) public nonReentrant {
+        require(
+            supportedMarkets[inputToken] != address(0),
+            UnsupportedMarket()
+        );
+        require(amount > 0, InvalidDepositAmount());
         require(
             IERC20(inputToken).transferFrom(msg.sender, address(this), amount),
             "Transfer failed"
         );
 
         userBalances[msg.sender][inputToken] += amount;
-        supportedAssetTypes[inputToken] = inputTokenType;
         emit Deposited(msg.sender, inputToken, amount);
     }
 
-    function convert(
-        address inputToken,
-        TokenType toAssetType
-    ) public returns (uint256) {
-        (
-            IStandardizedYield _SY,
-            IPPrincipalToken _PT,
-            IPYieldToken _YT
-        ) = IPMarket(supportedMarkets[inputToken]).readTokens();
-        TokenType fromTokenType = supportedAssetTypes[inputToken];
-        uint256 syAmount;
+    function withdraw(address withdrawToken) public nonReentrant {
+        require(withdrawToken != address(0), UnsupportedAsset());
         require(
-            fromTokenType != toAssetType,
-            "Input and output asset types cannot be the same"
+            supportedMarkets[withdrawToken] != address(0),
+            UnsupportedMarket()
         );
-        require(inputToken == address(0), "Token address cannot be 0");
+        require(
+            userBalances[msg.sender][withdrawToken] > 0,
+            InsufficientBalance()
+        );
+        require(
+            IERC20(withdrawToken).transfer(
+                msg.sender,
+                userBalances[msg.sender][withdrawToken]
+            ),
+            "Transfer failed"
+        );
+
+        userBalances[msg.sender][withdrawToken] = 0;
+        // emit Withdrawn(msg.sender, withdrawToken, userBalances[msg.sender][withdrawToken]);
+    }
+
+    function convert(address inputToken, TokenType toAssetType) public {
+        TokenType fromTokenType = supportedAssetTypes[inputToken];
+        address market = supportedMarkets[inputToken];
+        uint256 syAmount;
+        require(inputToken != address(0), UnsupportedAsset());
+        require(market != address(0), UnsupportedMarket());
         require(
             userBalances[msg.sender][inputToken] > 0,
-            "Amount in must be greater than 0"
+            InsufficientBalance()
+        );
+        IERC20(inputToken).forceApprove(
+            address(pendleRouter),
+            userBalances[msg.sender][inputToken]
         );
         if (fromTokenType == TokenType.UNDERLYING) {
-            uint256 netSyOut = pendleRouter.mintSyFromToken(
+            syAmount = pendleRouter.mintSyFromToken(
                 address(this),
-                address(_SY),
+                marketToken[market][TokenType.SY],
                 0,
                 createTokenInputSimple(
                     inputToken,
                     userBalances[msg.sender][inputToken]
                 )
             );
-            syAmount = netSyOut;
         } else if (fromTokenType == TokenType.LP) {
-            (uint256 netSYOut, ) = pendleRouter.removeLiquiditySingleSy(
+            (syAmount, ) = pendleRouter.removeLiquiditySingleSy(
                 address(this),
-                supportedMarkets[inputToken],
+                market,
                 userBalances[msg.sender][inputToken],
                 0,
                 createEmptyLimitOrderData()
             );
-            syAmount = netSYOut;
         } else if (fromTokenType == TokenType.SY) {
             syAmount = userBalances[msg.sender][inputToken];
         } else if (fromTokenType == TokenType.PT) {
-            (uint256 netSYOut, ) = pendleRouter.swapExactPtForSy(
+            (syAmount, ) = pendleRouter.swapExactPtForSy(
                 address(this),
                 supportedMarkets[inputToken],
                 userBalances[msg.sender][inputToken],
                 0,
                 createEmptyLimitOrderData()
             );
-            syAmount = netSYOut;
         } else if (fromTokenType == TokenType.YT) {
-            (uint256 netSYOut, ) = pendleRouter.swapExactYtForSy(
+            (syAmount, ) = pendleRouter.swapExactYtForSy(
                 address(this),
                 supportedMarkets[inputToken],
                 userBalances[msg.sender][inputToken],
                 0,
                 createEmptyLimitOrderData()
             );
-            syAmount = netSYOut;
         }
-
+        IERC20( marketToken[market][TokenType.SY]).forceApprove(
+            address(pendleRouter),
+            syAmount
+        );
         if (toAssetType == TokenType.UNDERLYING) {
-            (, address assetAddress,) = _SY.assetInfo();
+            (, address assetAddress, ) = IStandardizedYield(marketToken[market][TokenType.SY]).assetInfo();
             uint256 netTokenOut = pendleRouter.redeemSyToToken(
                 address(this),
-                address(_SY),
+                marketToken[market][TokenType.SY],
                 syAmount,
                 createTokenOutputSimple(assetAddress, 0)
             );
             userBalances[msg.sender][assetAddress] += netTokenOut;
         } else if (toAssetType == TokenType.SY) {
-            userBalances[msg.sender][address(_SY)] += syAmount;
+            userBalances[msg.sender][marketToken[market][TokenType.SY]] += syAmount;
         } else if (toAssetType == TokenType.PT) {
             (uint256 netPtOut, ) = pendleRouter.swapExactSyForPt(
                 address(this),
@@ -156,7 +189,7 @@ contract PendleSwap is Ownable, ReentrancyGuard {
                 createDefaultApproxParams(),
                 createEmptyLimitOrderData()
             );
-            userBalances[msg.sender][address(_PT)] += netPtOut;
+            userBalances[msg.sender][marketToken[market][TokenType.PT]] += netPtOut;
         } else if (toAssetType == TokenType.YT) {
             (uint256 netYtOut, ) = pendleRouter.swapExactSyForYt(
                 address(this),
@@ -166,109 +199,30 @@ contract PendleSwap is Ownable, ReentrancyGuard {
                 createDefaultApproxParams(),
                 createEmptyLimitOrderData()
             );
-            userBalances[msg.sender][address(_YT)] += netYtOut;
+            userBalances[msg.sender][marketToken[market][TokenType.YT]] += netYtOut;
+        } else if (toAssetType == TokenType.LP) {
+            (uint256 netLpOut, ) = pendleRouter.addLiquiditySingleSy(
+                address(this),
+                market,
+                syAmount,
+                0,
+                createDefaultApproxParams(),
+                createEmptyLimitOrderData()
+            );
+            userBalances[msg.sender][market] += netLpOut;
         }
         userBalances[msg.sender][inputToken] = 0;
-        return 0;
     }
 
-    // function swap(
-    //     address inputToken,
-    //     AssetType toAssetType,
-    //     uint256 amount
-    // ) public returns (uint256) {
-    //     AssetType fromTokenType = supportedAssetTypes[inputToken];
-    //     bytes4 selector;
-    //     bytes memory functionData;
-    //     require(
-    //         fromTokenType != toAssetType,
-    //         "Input and output asset types cannot be the same"
-    //     );
-    //     require(inputToken == address(0), "Token address cannot be 0");
-    //     require(
-    //         userBalances[msg.sender][inputToken] > 0,
-    //         "Amount in must be greater than 0"
-    //     );
 
-    //     if (fromTokenType == AssetType.UNDERLYING) {
-    //         if (toAssetType == AssetType.SY) {
-    //             selector = _getSelector(
-    //                 "mintSyFromToken(address,address,uint256,TokenInput)"
-    //             );
-    //         } else if (
-    //             toAssetType == AssetType.PT || toAssetType == AssetType.YT
-    //         ) {
-    //             selector = _getSelector(
-    //                 "mintPyFromToken(address,address,uint256,TokenInput)"
-    //             );
-    //         } else if (toAssetType == AssetType.LP) {
-    //             selector = _getSelector(
-    //                 "addLiquiditySingleToken(address,address,uint256,ApproxParams,TokenInput,LimitOrderData)"
-    //             );
-    //         }
-    //     } else if (fromTokenType == AssetType.SY) {
-    //         if (toAssetType == AssetType.UNDERLYING) {
-    //             selector = _getSelector(
-    //                 "redeemSyToToken(address,address,uint256,TokenOutput)"
-    //             );
-    //         } else if (
-    //             toAssetType == AssetType.PT || toAssetType == AssetType.YT
-    //         ) {
-    //             selector = _getSelector(
-    //                 "mintPyFromSy(address,address,uint256,uint256)"
-    //             );
-    //         } else if (toAssetType == AssetType.LP) {
-    //             selector = _getSelector(
-    //                 "addLiquiditySingleSy(address,address,uint256,ApproxParams,TokenInput,LimitOrderData)"
-    //             );
-    //         }
-    //     } else if (fromTokenType == AssetType.LP) {
-    //         if (toAssetType == AssetType.UNDERLYING) {
-    //             selector = _getSelector(
-    //                 "removeLiquiditySingleToken(address,address,uint256,ApproxParams,TokenOutput,LimitOrderData)"
-    //             );
-    //         } else if (toAssetType == AssetType.SY) {
-    //             selector = _getSelector(
-    //                 "removeLiquiditySingleToken(address,address,uint256,ApproxParams,TokenOutput,LimitOrderData)"
-    //             );
-    //         } else if (
-    //             toAssetType == AssetType.PT || toAssetType == AssetType.YT
-    //         ) {
-    //             selector = _getSelector(
-    //                 "mintPyFromToken(address,address,uint256,TokenInput)"
-    //             );
-    //         }
-    //     } else if (fromTokenType == AssetType.PT) {
-    //         if (toAssetType == AssetType.UNDERLYING) {
-    //             selector = _getSelector(
-    //                 "redeemPyToToken(address,address,uint256, TokenOutput)"
-    //             );
-    //         } else if (toAssetType == AssetType.SY) {
-    //             selector = _getSelector(
-    //                 "redeemPyToSy(address,address,uint256,uint256)"
-    //             );
-    //         } else if (toAssetType == AssetType.LP) {
-    //             selector = _getSelector(
-    //                 "addLiquiditySinglePt(address,address,uint256,ApproxParams,TokenInput,LimitOrderData)"
-    //             );
-    //         }
-    //     } else if (fromTokenType == AssetType.YT) {
-    //         if (toAssetType == AssetType.UNDERLYING) {
-    //             selector = _getSelector(
-    //                 "redeemPyToToken(address,address,uint256, TokenOutput)"
-    //             );
-    //         } else if (toAssetType == AssetType.SY) {
-    //             selector = _getSelector(
-    //                 "redeemYtToSy(address,address,uint256,uint256)"
-    //             );
-    //         } else if (toAssetType == AssetType.LP) {
-    //             selector = _getSelector(
-    //                 "addLiquiditySingleYt(address,address,uint256,ApproxParams,TokenInput,LimitOrderData)"
-    //             );
-    //         }
-    //     }
 
-    //     PENDLE_ROUTER.call(abi.encodePacked(selector, functionData));
-    //     return 0;
-    // }
+    function swap(
+        address inputToken,
+        TokenType toAssetType,
+        uint256 amount
+    ) public {
+        deposit(inputToken, amount);
+        convert(inputToken, toAssetType);
+        withdraw(marketToken[supportedMarkets[inputToken]][toAssetType]);
+    }
 }
--- test/PendleSwap.t.sol
+++ test/PendleSwap.t.sol
@@ -1,17 +1,138 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {PendleSwap} from "../src/PendleSwap.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IPMarket} from "pendle-core-v2-public/contracts/interfaces/IPMarket.sol";
+import {IStandardizedYield} from "pendle-core-v2-public/contracts/interfaces/IStandardizedYield.sol";
+import {IPPrincipalToken} from "pendle-core-v2-public/contracts/interfaces/IPPrincipalToken.sol";
+import {IPYieldToken} from "pendle-core-v2-public/contracts/interfaces/IPYieldToken.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PendleSwapTest is Test {
     PendleSwap public pendleSwap;
 
+    address public Bob = 0xe2fF3c4a7b87df9a6Acabf3a848083198080a763;
+    address public constant USDe = 0x4c9EDD5852cd905f086C759E8383e09bff1E68B3;
+    address public constant MARKET = 0xB451A36c8B6b2EAc77AD0737BA732818143A0E25;
+    address public constant SY = 0x4dB99b79361F98865230f5702de024C69f629fEC;
+    address public constant PT = 0x8A47b431A7D947c6a3ED6E42d501803615a97EAa;
+    address public constant YT = 0x4A8036EFA1307F1cA82d932C0895faa18dB0c9eE;
+    address public constant LP = 0xB451A36c8B6b2EAc77AD0737BA732818143A0E25;
+    uint256 public constant DEPOSIT_AMOUNT = 1_000e18;
+
+    mapping(PendleSwap.TokenType => address) public tokenAddresses;
+
     function setUp() public {
         pendleSwap = new PendleSwap();
+
+        // Setup supported assets
+        pendleSwap.setSupportedMarket(USDe, MARKET);
+
+        // Deal some tokens to the test contrac
+        deal(USDe, address(this), 10000000 * 10 ** 18); // 1M USDC
+        deal(SY, address(this), 10000000 * 10 ** 18);
+        deal(PT, address(this), 10000000 * 10 ** 18);
+        deal(YT, address(this), 10000000 * 10 ** 18);
+        deal(LP, address(this), 10000000 * 10 ** 18);
+
+        // Approve PendleSwap to spend tokens
+        IERC20(USDe).approve(address(pendleSwap), type(uint256).max);
+        IERC20(SY).approve(address(pendleSwap), type(uint256).max);
+        IERC20(PT).approve(address(pendleSwap), type(uint256).max);
+        IERC20(YT).approve(address(pendleSwap), type(uint256).max);
+        IERC20(LP).approve(address(pendleSwap), type(uint256).max);
+
+        tokenAddresses[PendleSwap.TokenType.UNDERLYING] = USDe;
+        tokenAddresses[PendleSwap.TokenType.SY] = SY;
+        tokenAddresses[PendleSwap.TokenType.PT] = PT;
+        tokenAddresses[PendleSwap.TokenType.YT] = YT;
+        tokenAddresses[PendleSwap.TokenType.LP] = LP;
     }
 
-    function test_PendleSwap() public view {
-        console.log("PendleSwap");
+    function test_setSupportedMarket() public {
+        vm.expectRevert(PendleSwap.UnsupportedAsset.selector);
+        pendleSwap.setSupportedMarket(address(0), MARKET);
+
+        assertEq(pendleSwap.supportedMarkets(USDe), MARKET);
+        assertEq(pendleSwap.supportedMarkets(SY), MARKET);
+        assertEq(pendleSwap.supportedMarkets(PT), MARKET);
+        assertEq(pendleSwap.supportedMarkets(YT), MARKET);
+        assertEq(pendleSwap.supportedMarkets(LP), MARKET);
+
+        vm.prank(Bob);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                Bob
+            )
+        );
+        pendleSwap.setSupportedMarket(address(USDe), MARKET);
+    }
+
+    function test_Deposit() public {
+        uint256 beforeBalance = IERC20(USDe).balanceOf(address(this));
+        pendleSwap.deposit(USDe, DEPOSIT_AMOUNT);
+        uint256 afterBalance = IERC20(USDe).balanceOf(address(this));
+        uint256 balance = pendleSwap.userBalances(address(this), USDe);
+        assertEq(afterBalance, beforeBalance - DEPOSIT_AMOUNT);
+        assertEq(balance, DEPOSIT_AMOUNT);
+
+        vm.expectRevert(PendleSwap.InvalidDepositAmount.selector);
+        pendleSwap.deposit(USDe, 0);
+
+        vm.expectRevert(PendleSwap.UnsupportedMarket.selector);
+        pendleSwap.deposit(address(0), DEPOSIT_AMOUNT);
+    }
+
+    function test_Withdraw() public {
+        uint256 beforeBalance = IERC20(USDe).balanceOf(address(this));
+        pendleSwap.deposit(USDe, DEPOSIT_AMOUNT);
+        pendleSwap.withdraw(USDe);
+        uint256 afterBalance = IERC20(USDe).balanceOf(address(this));
+        uint256 balance = pendleSwap.userBalances(address(this), USDe);
+        assertEq(afterBalance, beforeBalance);
+        assertEq(balance, 0);
+
+        vm.expectRevert(PendleSwap.InsufficientBalance.selector);
+        pendleSwap.withdraw(USDe);
+
+        vm.expectRevert(PendleSwap.UnsupportedAsset.selector);
+        pendleSwap.withdraw(address(0));
+
+        vm.expectRevert(PendleSwap.UnsupportedMarket.selector);
+        pendleSwap.withdraw(Bob);
+    }
+
+    function testFuzz_Convert(uint8 from, uint8 to) public {
+        PendleSwap.TokenType fromTokenType = PendleSwap.TokenType(from%5);
+        PendleSwap.TokenType toTokenType = PendleSwap.TokenType(to%5);
+        vm.assume(fromTokenType != toTokenType);
+        vm.assume(fromTokenType <= PendleSwap.TokenType.LP);
+        vm.assume(toTokenType <= PendleSwap.TokenType.LP);
+
+        pendleSwap.deposit(tokenAddresses[fromTokenType], DEPOSIT_AMOUNT);
+        pendleSwap.convert(tokenAddresses[fromTokenType], toTokenType);
+        assertEq(pendleSwap.userBalances(address(this), tokenAddresses[fromTokenType]), 0);
+        assertGt(pendleSwap.userBalances(address(this), tokenAddresses[toTokenType]), 0);
+
+        vm.expectRevert(PendleSwap.UnsupportedAsset.selector);
+        pendleSwap.convert(address(0), toTokenType);
+
+        vm.expectRevert(PendleSwap.UnsupportedMarket.selector);
+        pendleSwap.convert(Bob, toTokenType);
+    }
+
+    function testFuzz_Swap(uint8 from, uint8 to) public {
+        PendleSwap.TokenType fromTokenType = PendleSwap.TokenType(from%5);
+        PendleSwap.TokenType toTokenType = PendleSwap.TokenType(to%5);
+        vm.assume(fromTokenType != toTokenType);
+        vm.assume(fromTokenType <= PendleSwap.TokenType.LP);
+        vm.assume(toTokenType <= PendleSwap.TokenType.LP);
+
+        pendleSwap.swap(tokenAddresses[fromTokenType], toTokenType, DEPOSIT_AMOUNT);
+        assertGt(IERC20(tokenAddresses[toTokenType]).balanceOf(address(this)), 0);
+        
     }
 }
